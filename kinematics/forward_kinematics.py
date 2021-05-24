9df139fbc7a8072dcd02e48004b54939f28c16ed
Ammar594 <a.94@hotmail.co.uk>
2021-05-24 23:09:13 +0200

    submission forward kinematics

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -1,5 +1,4 @@
 '''In this exercise you need to implement forward kinematics for NAO robot
-
 * Tasks:
     1. complete the kinematics chain definition (self.chains in class ForwardKinematicsAgent)
        The documentation from Aldebaran is here:
@@ -10,7 +9,6 @@
        http://doc.aldebaran.com/2-1/family/nao_h21/links_h21.html
     3. complete function ForwardKinematicsAgent.forward_kinematics, save the transforms of all body parts in torso
        coordinate into self.transforms of class ForwardKinematicsAgent
-
 * Hints:
     the local_trans has to consider different joint axes and link parameters for different joints
 '''
@@ -22,7 +20,12 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '
 
 from numpy.matlib import matrix, identity
 
-from angle_interpolation import AngleInterpolationAgent
+from joint_control.angle_interpolation import AngleInterpolationAgent
+import math
+import numpy as np
+from joint_control.keyframes import hello,rightBackToStand,rightBellyToStand,leftBellyToStand
+
+
 
 
 class ForwardKinematicsAgent(AngleInterpolationAgent):
@@ -35,9 +38,20 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         self.transforms = {n: identity(4) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
-        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
-                       # YOUR CODE HERE
+        self.chains = {'Head': ['HeadYaw', 'HeadPitch'],
+                       'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll'],
+                       'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],
+                       'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll'],
+                       'RArm': ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll']
                        }
+        self.joint_offsets = {
+                        'RLeg': [[0, -50, -85], [0, 0, 0], [0, 0, 0], [0, 0, -100], [0, 0, -102.9], [0, 0, 0]],
+                        'RArm': [[0, -98, 100], [0, 0, 0], [105, 15, 0], [0, 0, 0]],
+                        'LArm': [[0, 98, 100], [0, 0, 0], [105, 15, 0], [0, 0, 0]],
+                        'LLeg': [[0, 50, -85], [0, 0, 0], [0, 0, 0], [0, 0, -100], [0, 0, -102.9], [0, 0, 0]],
+                        'Head': [[0, 0, 126.5], [0, 0, 0]]}
+
+        
 
     def think(self, perception):
         self.forward_kinematics(perception.joint)
@@ -45,31 +59,45 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
 
     def local_trans(self, joint_name, joint_angle):
         '''calculate local transformation of one joint
-
         :param str joint_name: the name of joint
         :param float joint_angle: the angle of joint in radians
         :return: transformation
         :rtype: 4x4 matrix
         '''
         T = identity(4)
-        # YOUR CODE HERE
 
-        return T
+        if joint_name in ["HeadYaw", "LElbowYaw", "RElbowYaw", "LHipYawPitch", "RHipYawPitch"]:
+            result = np.dot(T, np.array([[np.cos(joint_angle), -np.sin(joint_angle), 0, 0], [np.sin(joint_angle),
+                                                                                        np.cos(joint_angle), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]))
+        elif joint_name.endswith("Pitch"):
+            result = np.dot(T, np.array([[np.cos(joint_angle), 0, np.sin(joint_angle), 0], [0, 1, 0, 0], [-np.sin(joint_angle), 0,
+                                                                                                     np.cos(joint_angle), 0], [0, 0, 0, 1]]))
+        elif joint_name.endswith("Roll"):
+            result = np.dot(T, np.array([[1, 0, 0, 0], [0, np.cos(joint_angle), -np.sin(joint_angle), 0], [0, np.sin(joint_angle),
+                                                                                                      np.cos(joint_angle), 0], [0, 0, 0, 1]]))
+
+        for i in self.chains.keys():
+            if joint_name in self.chains[i]:
+                result[0, 3] = self.joint_offsets[i][self.chains[i].index(joint_name)][0]
+                result[1, 3] = self.joint_offsets[i][self.chains[i].index(joint_name)][1]
+                result[2, 3] = self.joint_offsets[i][self.chains[i].index(joint_name)][2]
+        return result
 
     def forward_kinematics(self, joints):
-        '''forward kinematics
-
+        '''forward kinematicschain
         :param joints: {joint_name: joint_angle}
         '''
+        print(joints)
         for chain_joints in self.chains.values():
             T = identity(4)
             for joint in chain_joints:
                 angle = joints[joint]
                 Tl = self.local_trans(joint, angle)
-                # YOUR CODE HERE
-
+                T = np.dot(T, Tl)
                 self.transforms[joint] = T
 
 if __name__ == '__main__':
     agent = ForwardKinematicsAgent()
+    agent.keyframes = leftBellyToStand()
+
     agent.run()
